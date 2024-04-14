269b7d5b841b5c4451ca587e1810c24ede866c27
AmeWenJ <115850592+AmeWenJ@users.noreply.github.com>
2024-04-14 08:59:14 +0800

    Update hazardAvoidancePass.cpp
    Finished check for subroutine call, subroutine return, and I/O instruction.

--- hazardAvoidance/hazardAvoidancePass/hazardAvoidancePass.cpp
+++ hazardAvoidance/hazardAvoidancePass/hazardAvoidancePass.cpp
@@ -2,9 +2,17 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Analysis/BlockFrequencyInfo.h"
+#include "llvm/Analysis/BranchProbabilityInfo.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
 
-#include  <iostream>
-
+#include <unordered_set>
 using namespace llvm;
 
 namespace {
@@ -13,7 +21,39 @@ struct hazardAvoidancePass : public PassInfoMixin<hazardAvoidancePass> {
 
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
 
-    std::cout << "Hello function: " << F.getName().str() << std::endl;
+    std::unordered_set<BasicBlock*> SuperBlockBB;
+
+    for (BasicBlock &BB : F) {
+        bool goodBB = true;
+
+        for (Instruction &I : BB) {
+
+          if (auto *Call = dyn_cast<CallInst>(&I)){
+                goodBB = false;
+                break;
+                //errs() << "This basic block contains an I/O or function call or function return\n";
+          }
+          else if (auto *Load = dyn_cast<LoadInst>(&I)){
+                Type *OpType = Load->getPointerOperandType();
+                if (OpType->isPointerTy()){
+                    Value *OpValue = Load->getPointerOperand();
+                    if (auto *OpInst = dyn_cast<Instruction>(OpValue)){
+                        if (isa<CallInst>(OpInst)){
+                            goodBB = false;
+                            break;
+                            //errs() << "This basic block contains an I/O or function return\n";
+                        }
+                    }
+
+                }
+          }
+        }
+
+        if (goodBB){
+          SuperBlockBB.insert(&BB);
+        }
+    }
+
     return PreservedAnalyses::all();
   }
 };
